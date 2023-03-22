db4e89e9ae9e8940bc5565f07c41f447fd407639
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2023-03-22 16:31:09 -0600

    Creando subcomponentes en el MF

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from './Button';
 
 import "./Counter.css"
 
 const Counter = () => {
+
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className='Counter-container'>Counter</div>
+    <div className='Counter-container'>
+      <h2>Counter: {counter}</h2>
+      <Button onClick={() => alert("click")} text="+1" />
+      <Button onClick={() => alert("click")} text="-1" />
+    </div>
   )
 }
 
